6d06b65eefcb74ee4a48f43750d91210dd15e410
MOHAMMAD SHAMOON <ms12736@nyu.edu>
2022-06-26 23:12:49 -0400

    work in progress

---
+++ my-app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import './index.css';
+
+class Square extends React.Component {
+    render() {
+      return (
+        <button className="square">
+          {this.props.value}
+        </button>
+      );
+    }
+  }
+
+class Board extends React.Component {
+    renderSquare(i) {
+      return <Square value={i} />;
+    }
+  }
